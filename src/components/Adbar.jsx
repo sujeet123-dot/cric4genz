01e737dc6ed93bb3df7009e14e5dddff8880278e
sujeet123-dot <sujeetkumarak40@gmail.com>
2026-03-21 17:54:23 +0530

    added css

--- src/components/Adbar.jsx
+++ src/components/Adbar.jsx
@@ -6,47 +6,31 @@ const CleverAds = () => {
 
     const script = document.createElement("script");
     script.id = "CleverCoreLoader100674";
+    script.src = "https://scripts.cleverwebserver.com/bb8bbc863573a5296f490916b1afeca1.js";
     script.async = true;
     script.type = "text/javascript";
     script.setAttribute("data-cfasync", "false");
 
-    script.innerHTML = `
-      (function (document, window) {
-        var a, c = document.createElement("script"), f = window.frameElement;
-
-        c.id = "CleverCoreLoader100674-inner";
-        c.src = "https://scripts.cleverwebserver.com/bb8bbc863573a5296f490916b1afeca1.js";
-        c.async = true;
-        c.type = "text/javascript";
-
-        c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
-
-        try {
-          a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
-        } catch (e) {
-          a = false;
-        }
-
-        a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
-        a.parentNode.insertBefore(c, a);
-      })(document, window);
-    `;
-
     document.body.appendChild(script);
   }, []);
 
-  return <div
+  return (
+    <div
+      id="clever-ad-container"
       style={{
         width: "100%",
-        height: height,
+        minHeight: "250px",   // ✅ FIXED
+        margin: "20px 0",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         overflow: "visible",
         position: "relative",
-        zIndex: 9999
+        zIndex: 9999,
+        background: "#fff"
       }}
-    />;
+    />
+  );
 };
 
 export default CleverAds;
