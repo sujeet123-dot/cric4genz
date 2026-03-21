69e8895e8f5b40a37aa27c70ae2a071a951dd9c8
sujeet123-dot <sujeetkumarak40@gmail.com>
2026-03-21 17:29:34 +0530

    fix issue

--- src/components/Adbar.jsx
+++ src/components/Adbar.jsx
@@ -2,26 +2,40 @@ import { useEffect } from "react";
 
 const CleverAds = () => {
   useEffect(() => {
-    const script = document.createElement("script");
+    if (document.getElementById("CleverCoreLoader100674")) return;
 
+    const script = document.createElement("script");
     script.id = "CleverCoreLoader100674";
-    script.src = "https://scripts.cleverwebserver.com/bb8bbc863573a5296f490916b1afeca1.js";
     script.async = true;
     script.type = "text/javascript";
+    script.setAttribute("data-cfasync", "false");
 
-    script.setAttribute("data-target", window.name || "");
-    script.setAttribute("data-callback", "yourCallbackFunction");
-    script.setAttribute("data-callback-url-click", "yourClickMacro");
-    script.setAttribute("data-callback-url-view", "yourViewMacro");
+    script.innerHTML = `
+      (function (document, window) {
+        var a, c = document.createElement("script"), f = window.frameElement;
+
+        c.id = "CleverCoreLoader100674-inner";
+        c.src = "https://scripts.cleverwebserver.com/bb8bbc863573a5296f490916b1afeca1.js";
+        c.async = true;
+        c.type = "text/javascript";
+
+        c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
+
+        try {
+          a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
+        } catch (e) {
+          a = false;
+        }
+
+        a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
+        a.parentNode.insertBefore(c, a);
+      })(document, window);
+    `;
 
     document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script); // cleanup
-    };
   }, []);
 
-  return <div id="ad-container"></div>;
+  return <div style={{ minHeight: "250px" }}></div>;
 };
 
 export default CleverAds;
